0052ff84a2171dc1711bc393a18672e81f0b4415
bharti <bharti@webmobsoft.com>
2024-08-01 20:01:28 +0530

    ticket screen changes

--- lib/screens/TicketScreen.dart
+++ lib/screens/TicketScreen.dart
@@ -25,8 +25,11 @@ class TicketScreen extends StatelessWidget {
               child: Container(
                 margin: EdgeInsets.only(left: 0),
                 child: RichText(
-                  text: const TextSpan(
-                    style: TextStyle(color: Colors.white, fontSize: 50),
+                  text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: getResponsiveFontSize(
+                            screenWidth, screenHeight, 50)),
                     children: [
                       TextSpan(
                         text: "Your  ",
@@ -48,7 +51,7 @@ class TicketScreen extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Container(
-                  width: double.infinity, // Take full width of parent
+                  width: double.infinity,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     gradient: const LinearGradient(
@@ -64,92 +67,100 @@ class TicketScreen extends StatelessWidget {
                       vertical:
                           screenHeight * 0.02, // Responsive vertical padding
                     ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            RichText(
-                              text: TextSpan(
+                    child: IntrinsicHeight(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: getResponsiveFontSize(
+                                          screenWidth, screenHeight, 20)),
+                                  children: [
+                                    TextSpan(
+                                        text: "XTRA",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: getResponsiveFontSize(
+                                                screenWidth,
+                                                screenHeight,
+                                                30))),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                "17.10.2024",
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontSize: getResponsiveFontSize(
-                                        screenWidth, screenHeight, 20)),
-                                children: [
-                                  TextSpan(
-                                      text: "XTRA",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: getResponsiveFontSize(
-                                              screenWidth, screenHeight, 30))),
-                                ],
+                                        screenWidth, screenHeight, 20),
+                                    fontWeight: FontWeight.w100),
                               ),
-                            ),
-                            Text(
-                              "17.10.2024",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: getResponsiveFontSize(
-                                      screenWidth, screenHeight, 20),
-                                  fontWeight: FontWeight.w100),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: screenHeight *
-                                0.01), // Space between row and text
-                        Text(
-                          "1 active ticket",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: getResponsiveFontSize(
-                                  screenWidth, screenHeight, 20),
-                              fontWeight: FontWeight.w100,
-                              fontStyle: FontStyle.italic),
-                        ),
-                        SizedBox(
-                            height: screenHeight *
-                                0.02), // Space within gradient container
-                      ],
+                            ],
+                          ),
+                          SizedBox(
+                              height: screenHeight *
+                                  0.01), // Space between row and text
+                          Text(
+                            "1 active ticket",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: getResponsiveFontSize(
+                                    screenWidth, screenHeight, 20),
+                                fontWeight: FontWeight.w100,
+                                fontStyle: FontStyle.italic),
+                          ),
+                          SizedBox(
+                              height: screenHeight *
+                                  0.02), // Space within gradient container
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
 
-            SizedBox(
-              width: screenWidth * 0.5, // Adjust width as needed
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(
-                        vertical: screenHeight * 0.02), // Responsive padding
-                    backgroundColor: Color.fromARGB(255, 117, 116, 116),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.white, width: 2),
+            SizedBox(height: screenHeight * 0.01),
+            Center(
+              child: SizedBox(
+                width: screenWidth * 0.5, // Adjust width as needed
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: screenHeight * 0.02), // Responsive padding
+                      backgroundColor: Color.fromARGB(255, 117, 116, 116),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: BorderSide(color: Colors.white, width: 2),
+                      ),
                     ),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'Past Tickets!',
-                            style: TextStyle(
-                              fontFamily: "Helvetica",
-                              color: Colors.white,
-                              fontSize: 23,
-                              fontWeight: FontWeight.w500,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Past Tickets!',
+                              style: TextStyle(
+                                fontFamily: "Helvetica",
+                                color: Colors.white,
+                                fontSize: getResponsiveFontSize(
+                                    screenWidth, screenHeight, 23),
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
-                          ),
-                          SizedBox(width: 10),
-                        ],
+                            SizedBox(width: 10),
+                          ],
+                        ),
                       ),
                     ),
                   ),
